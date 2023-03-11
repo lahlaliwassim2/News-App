5d6407c33234be22631bfd160bcbc7a31a6fde20
lahlaliwassim2 <wlahlali34@gmail.com>
2023-03-11 18:25:09 +0100

    ajouter la navigation

---
+++ NewsApp/components/NewsOverView.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NewsOverView = () => {
+  return (
+    <View>
+      <Text>NewsOverView</Text>
+    </View>
+  )
+}
+
+export default NewsOverView
+
+const styles = StyleSheet.create({})
