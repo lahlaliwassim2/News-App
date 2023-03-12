439037a0407089ea61953d425ee3976f502d9380
lahlaliwassim2 <wlahlali34@gmail.com>
2023-03-12 15:15:21 +0100

    ajouter la paritie des boxes pour le filtrage par categorie

--- NewsApp/screens/Home.tsx
+++ NewsApp/screens/Home.tsx
@@ -1,14 +1,34 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import {Appbar}  from 'react-native-paper'
+import React, { useState } from 'react'
+import {Appbar, Chip}  from 'react-native-paper'
+const categories =["Technologie", "Sport","Politics","Health","Buisnes"]
+const API_KEY = "pub_187635d956506d548fdfafd33df7fe91981ef&country=ma"
 const Home = () => {
+  const [selectedCategories, setselectedCategories] = useState([])
+  const handelSelect = (val: string)=> {
+    setselectedCategories((prev:string[])  => 
+    prev.find((p) => p===val)?prev.filter((cat) => cat !==val) 
+: [...prev, val] )
+  }
   return (
     <View style={styles.container}>
      <Appbar.Header>
         <Appbar.Content title="Home"></Appbar.Content>
      </Appbar.Header>
      <View style={styles.filterContainer}>
-
+    {categories.map((cat)=>( 
+    <Chip
+        key={cat} 
+        mode="outlined" 
+        style={styles.chipItem} 
+        textStyle={{fontWeight:"400",color:"white",padding:1 }}
+        showSelectedOverlay
+        selected={selectedCategories.find((c)=>cat === c) ? true : false }
+        onPress={()=>handelSelect(cat)}
+        >
+          {cat}
+    </Chip>
+    ))}
      </View>
     </View>
   )
@@ -24,6 +44,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap:"wrap",
     marginVertical:10,
-
+  },
+  chipItem : {
+    marginHorizontal:5,
+    marginVertical: 5 
   }
 })
